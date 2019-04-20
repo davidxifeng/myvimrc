a7d846eddd64a3887ed2605943edb543447f17a4
David Feng <davidxifeng@gmail.com>
2019-04-20 17:31:29 +0900

    fix

--- zshrc_on_macos.sh
+++ zshrc_on_macos.sh
@@ -1,3 +1,11 @@
+set -o vi
+
+export HOMEBREW_NO_AUTO_UPDATE=TRUE
+
+DISABLE_UNTRACKED_FILES_DIRTY="true"
+# use this for repo which slow down zsh
+# git config --add oh-my-zsh.hide-dirty 1
+
 ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="xiong-chiamiov-plus"
 plugins=(git brew cabal sublime)
@@ -10,43 +18,24 @@ export LANG=en_US.UTF-8
 
 export EDITOR=nvim
 
+export PATH=/usr/local/sbin:$PATH
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH=~/bin:$PATH
 export PATH=~/.cabal/bin:$PATH
-export PATH=~/Library/Python/2.7/bin:$PATH
-export PATH=~/.gem/ruby/2.0.0/bin:$PATH
 export PATH=~/Library/Android/sdk/platform-tools:$PATH
 export PATH=~/work/flutter-space/flutter/bin:$PATH
 
+# some pip local installed apps
+export PATH=~/Library/Python/2.7/bin:$PATH
 
-# 使用通过brew安装的gnu 版core utils
-#export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
-#export PATH=~/.neovim/bin:$PATH
-#export PATH=~/openresty/bin:~/openresty/nginx/sbin:$PATH
-#export PATH=/opt/bochs/bin:$PATH
 export NODE_PATH="/usr/local/lib/node_modules"
-
-# oracle java 1.8
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
-
-# uncomment when needed
-#export BXSHARE="/opt/bochs/share/bochs"
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8) # oracle java 1.8
 
 # env LUA_PATH : package.path, LUA_CPATH
 export LUAPATH=~/david/gl/lua/src
 export LUADIR=~/david/gl/lua/src
 
 export VIS_PATH=~/david/vis
-
-export HOMEBREW_NO_AUTO_UPDATE=TRUE
-
-# dirty hack
-
-__git_files () {
-    _wanted files expl 'local files' _files
-}
-
-set -o vi
 
 export NVM_DIR="$HOME/.nvm"
 source "/usr/local/opt/nvm/nvm.sh"
